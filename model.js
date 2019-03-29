929d309c0839132578a29710afa23e7d8a9bf107
MeganSmith <46550038+meganmsmith58@users.noreply.github.com>
2019-03-29 12:00:23 -0600

    created-modalJS_added-style

---
+++ model.js
@@ -0,0 +1,15 @@
+
+class PlayerCollection {
+    constructor() {
+
+    }
+}
+
+
+class Players {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+
+    }
+}
--- script.js
+++ script.js
@@ -8,11 +8,10 @@ function getCourses() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let allCourses = JSON.parse(this.responseText);
-            console.log(allCourses);
+            //console.log(allCourses);
             for (let c = 0; c < allCourses.courses.length; c++) {
                 $('.courseBox').append(`<div class="coursesList">
-                    <button onclick="selectCourse(${allCourses.courses[c].id}, this)">${allCourses.courses[c].name}</button>
-                   
+                    <button class="courseBtn" onclick="selectCourse(${allCourses.courses[c].id}, this)">${allCourses.courses[c].name}</button>
                     </div>`);
             }
         }
@@ -28,10 +27,10 @@ function selectCourse(id, btn) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             selectedCourse = JSON.parse(this.responseText);
-            console.log(selectedCourse);
+            //console.log(selectedCourse);
 
             let teeBox = selectedCourse.data.holes[0].teeBoxes;
-            console.log(teeBox);
+            //console.log(teeBox);
 
             for (let i = 0; i < teeBox.length; i++) {
                 $(theCourse).append(`<button onclick="showCard(${i})">${teeBox[i].teeColorType}</button>`)
@@ -44,11 +43,40 @@ function selectCourse(id, btn) {
 
 function showCard(teeType) {
     let selectedTees = teeType;
-    $('.courseCard').append(`<div id="courseName">${selectedCourse.data.name}</div>`)
+    $('.courseCard').append(`<div id="courseName">${selectedCourse.data.name}</div>`);
     for (let c = 0; c < selectedCourse.data.holes.length; c++) {
-        $('.courseCard').append(`<div>
-            <div>Hole ${selectedCourse.data.holes[c].hole}</div>
-            <div>Par ${selectedCourse.data.holes[c].teeBoxes[selectedTees].par}</div>
+        $('.courseInfoBox').append(`<div id="col${c}" class="columns">
+           
+            <div class="holeRow, holeStyle">${selectedCourse.data.holes[c].hole}</div>
+            <div class="holeRow, parStyle">${selectedCourse.data.holes[c].teeBoxes[selectedTees].par}</div>
+            <div class="holeRow, yardsStyle">${selectedCourse.data.holes[c].teeBoxes[selectedTees].yards}</div>
+            <div class="holeRow, hcpStyle">${selectedCourse.data.holes[c].teeBoxes[selectedTees].hcp}</div>
             </div>`)
+    }
+    $('.holeParYardBox').append(`<div>
+            <div class="holeStyle">hole</div>
+            <div class="parStyle">par</div>
+            <div class="yardsStyle">yards</div>
+            <div class="hcpStyle">handicap</div>
+            </div>`)
+    addScoreBoxes();
+}
+
+
+function addScoreBoxes() {
+    let players = 4;
+    $('.playerBox').append(`<div>
+        <!--<div>hole</div>-->
+        <!--<div>par</div>-->
+        <!--<div>yards</div>-->
+        <input type="text" placeholder="Player 1">
+        <input type="text" placeholder="Player 1">
+        <input type="text" placeholder="Player 1">
+        <input type="text" placeholder="Player 1">
+        </div>`)
+    for (let p = 0; p < players; p++) {
+        for (let h = 0; h < selectedCourse.data.holes.length; h++) {
+            $('#col' + h).append(`<input class="scoreInputBox" id="p${p}h${h}" type="text">`)
+        }
     }
 }
